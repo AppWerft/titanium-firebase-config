832c5a539e756b7372846c2dce14a2616fd79ed2
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2018-01-11 14:11:33 +0100

    BuildConfig for debugging

--- android/src/firebase/config/FirebaseconfigModule.java
+++ android/src/firebase/config/FirebaseconfigModule.java
@@ -12,11 +12,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.appcelerator.kroll.KrollDict;
+import org.appcelerator.kroll.KrollFunction;
 import org.appcelerator.kroll.KrollModule;
 import org.appcelerator.kroll.annotations.Kroll;
-import org.appcelerator.titanium.TiApplication;
 import org.appcelerator.kroll.common.Log;
 import org.appcelerator.kroll.common.TiConfig;
+import org.appcelerator.titanium.TiApplication;
+import android.support.annotation.NonNull;
+import android.app.Activity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
 @Kroll.module(name = "Firebaseconfig", id = "firebase.config")
 public class FirebaseconfigModule extends KrollModule {
@@ -24,7 +38,9 @@ public class FirebaseconfigModule extends KrollModule {
 	// Standard Debugging variables
 	private static final String LCAT = "FBconfig";
 	private static final boolean DBG = TiConfig.LOGD;
-
+	private static FirebaseRemoteConfig firebaseRemoteConfig;
+	private KrollFunction onLoad = null;
+	
 	// You can define constants with @Kroll.constant, for example:
 	@Kroll.constant
 	public static final int FETCH_STATUS_NO_FETCH_YET = 0;
@@ -48,18 +64,45 @@ public class FirebaseconfigModule extends KrollModule {
 	@Kroll.onAppCreate
 	public static void onAppCreate(TiApplication app) {
 		Log.d(LCAT, "inside onAppCreate");
-		// put module init code that needs to run when the application is
-		// created
+		firebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+		
 	}
 
+	@Kroll.method
+	public void setDeveloperModeEnabled() {
+		FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+        .setDeveloperModeEnabled(BuildConfig.DEBUG)
+        .build();
+firebaseRemoteConfig.setConfigSettings(configSettings);
+	}
+	
 	@Kroll.method
 	public void activateFetched() {
-
+		firebaseRemoteConfig.activateFetched();
 	}
-
 	@Kroll.method
 	public void fetch(KrollDict props) {
-
+		
+		int expirationDuration = 0;
+		if (props.containsKeyAndNotNull("load")) {
+			onLoad = (KrollFunction)props.get("load");
+		}
+		if (props.containsKeyAndNotNull("expirationDuration")) {
+			expirationDuration = props.getInt("expirationDuration");
+		}
+		Activity activity = TiApplication.getAppRootOrCurrentActivity();
+		firebaseRemoteConfig.fetch().addOnCompleteListener(activity, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful() && onLoad !=  null) {
+                    onLoad.callAsync(getKrollObject(), new KrollDict());
+                    firebaseRemoteConfig.activateFetched();
+                } else {
+                   
+                }
+               
+            }
+        });
 	}
 
 	@Kroll.method
@@ -92,7 +135,7 @@ public class FirebaseconfigModule extends KrollModule {
 
 	public void setDefaults(KrollDict defaults,
 			@Kroll.argument(optional = true) String nameSpace) {
-
+		firebaseRemoteConfig.setDefaults(defaults);
 	}
 
 	public KrollDict defaultValueForKey(String key) {
